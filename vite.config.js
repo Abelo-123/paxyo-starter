046c69b9b8c0d3d9b357bc4e0a55fd06ab1a0ed0
abel <abeloabate01@gmail.com>
2025-05-09 17:57:49 +0300

    ps-d

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  // vite.config.j
   base: '/paxyo-starter/', // Replace 'broke' with your repo name
   build: {
     outDir: 'dist', // Ensure the build output is to the 'dist' folder
   },
-
-
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Add alias for tgui-core
+      'tgui-core': path.resolve(__dirname, 'node_modules/tgui-core'),
+    },
+  },
+});
